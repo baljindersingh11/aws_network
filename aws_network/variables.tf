2fb4db847bef445f0cf96130231270024d6ffea5
baljindersingh11 <130147849+baljindersingh11@users.noreply.github.com>
2025-06-13 21:24:58 -0400

    Update variables.tf
    removing the default cidr

--- aws_network/variables.tf
+++ aws_network/variables.tf
@@ -17,14 +17,14 @@ variable "prefix" {
 
 # Provision public subnets in custom VPC
 variable "private_cidr_blocks" {
-  default     = ["10.1.0.0/24", "10.1.1.0/24"]
+ # default     = ["10.1.0.0/24", "10.1.1.0/24"]
   type        = list(string)
   description = "Public Subnet CIDRs"
 }
 
 # VPC CIDR range
 variable "vpc_cidr" {
-  default     = "10.1.0.0/16"
+#  default     = "10.1.0.0/16"
   type        = string
   description = "VPC to host static web site"
 }
