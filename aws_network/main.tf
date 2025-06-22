17526b204164ad3db618fde059b3ae63d3b14f90
EC2 Default User <ec2-user@ip-172-31-47-232.ec2.internal>
2025-06-22 22:24:42 +0000

    change

--- aws_network/main.tf
+++ aws_network/main.tf
@@ -61,7 +61,7 @@ resource "aws_subnet" "private_subnet" {
     }
   )
 }
-
+/*
 # Create Internet Gateway
 resource "aws_internet_gateway" "igw" {
   vpc_id = aws_vpc.main.id
@@ -124,20 +124,7 @@ resource "aws_nat_gateway" "nat-gw" {
   }
   depends_on    = [aws_internet_gateway.igw]
 }
-/*
-resource "aws_route_table" "private" {
-  vpc_id = aws_vpc.main.id
 
-  route {
-    cidr_block     = "0.0.0.0/0"
-    nat_gateway_id = aws_nat_gateway.nat.id
-  }
-
-  tags = {
-    Name = "Private Route Table"
-  }
-}
-*/
 
 resource "aws_route_table" "private_route_table" {
   vpc_id = aws_vpc.main.id
@@ -151,4 +138,81 @@ resource "aws_route_table_association" "private_route_table_association" {
   route_table_id = aws_route_table.private_route_table.id
   subnet_id      = aws_subnet.private_subnet.id
   
+}
+
+*/
+
+# Create Internet Gateway
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.main.id
+
+  tags = merge(var.default_tags,
+    {
+      "Name" = "${var.prefix}-igw"
+    }
+  )
+}
+
+# Route table to route add default gateway pointing to Internet Gateway (IGW)
+resource "aws_route_table" "public_route_table" {
+  vpc_id = aws_vpc.main.id
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+  tags = {
+    Name = "${var.prefix}-route-public-route_table"
+  }
+}
+
+# Associate subnets with the custom route table
+resource "aws_route_table_association" "public_route_table_association" {
+  count          = length(aws_subnet.public_subnet[*].id)
+  route_table_id = aws_route_table.public_route_table.id
+  subnet_id      = aws_subnet.public_subnet[count.index].id
+}
+
+#Create NAT GW
+resource "aws_nat_gateway" "nat-gw" {
+  allocation_id = aws_eip.nat-eip.id
+  subnet_id     = aws_subnet.public_subnet[0].id
+
+  tags = {
+    Name = "${var.prefix}-natgw"
+  }
+
+  # To ensure proper ordering, it is recommended to add an explicit dependency
+  # on the Internet Gateway for the VPC.
+  depends_on = [aws_internet_gateway.igw]
+}
+
+# Create elastic IP for NAT GW
+resource "aws_eip" "nat-eip" {
+  vpc   = true
+  tags = {
+    Name = "${var.prefix}-natgw"
+  }
+
+}
+
+# Route table to route add default gateway pointing to NAT Gateway (NATGW)
+resource "aws_route_table" "private_route_table" {
+  vpc_id = aws_vpc.main.id
+  tags = {
+    Name = "${var.prefix}-route-private-route_table",
+    Tier = "Private"
+  }
+}
+
+# Add route to NAT GW if we created public subnets
+resource "aws_route" "private_route" {
+  route_table_id         = aws_route_table.private_route_table.id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = aws_nat_gateway.nat-gw.id
+}
+
+# Associate subnets with the custom route table
+resource "aws_route_table_association" "private_route_table_association" {
+  route_table_id = aws_route_table.private_route_table.id
+  subnet_id      = aws_subnet.private_subnet.id
 }
