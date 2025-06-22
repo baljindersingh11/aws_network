4519107852b4b542e3abc0b8e7c4285db91f9623
EC2 Default User <ec2-user@ip-172-31-47-232.ec2.internal>
2025-06-22 21:02:06 +0000

    Add output variable to globalvars module

--- globalvars/output.tf
+++ globalvars/output.tf
@@ -1,12 +1,12 @@
 # Default tags
 output "default_tags" {
   value = {
-    "Owner" = "Irina"
+    "Owner" = "Baljinder"
     "App"   = "Web"
   }
 }
 
 # Prefix to identify resources
 output "prefix" {
-  value     = "week5"
+  value     = "week6"
 }
